6c57e7f634901de4ddb6c9ab23af333bbf6c9ada
sphnikola <sphnikola@gmail.com>
2023-12-06 01:55:05 +0000

    add default styles for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        grotesk: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        Red: "hsl(0, 100%, 66%)",
+        White: "hsl(0, 0%, 100%)",
+        "Light-grayish-violet": "hsl(270, 3%, 87%)",
+        "Dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "Very-dark-violet": "hsl(278, 68%, 11%)",
+      },
+    },
   },
   plugins: [],
 };
