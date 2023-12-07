e604dde111e006e30e9b9e7bc89aba859ab9bce0
sphnikola <sphnikola@gmail.com>
2023-12-07 15:56:21 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="font-grotesk">
+        <h1 className="underline">hello world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
