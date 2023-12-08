f7cf1df18e9290b43380de0352c388a1ab71d86c
sphnikola <sphnikola@gmail.com>
2023-12-08 09:48:11 +0000

    Add front card for mobile without initial info displayed on it

--- src/LeftSection.jsx
+++ src/LeftSection.jsx
@@ -1,8 +1,30 @@
+import card_logo from "./assets/images/card-logo.svg";
+import card_front from "./assets/images/bg-card-front.png";
+
 export default function LeftSection() {
   return (
     <div
       className=" w-full min-h-[30vh] bg-bg-main-mobile bg-center bg-cover bg-no-repeat relative
     md:min-h-screen md:w-[40%] md:bg-bg-main-desktop "
-    ></div>
+    >
+      {/* container for the cards  */}
+      <div className="flex flex-col-reverse absolute left-[61%] top-[60%] -translate-x-[50%] -translate-y-[50%]">
+        {/* front card */}
+        <div className="z-50 w-[280px] relative -translate-x-[100px]">
+          {/* card logo */}
+          <img
+            src={card_logo}
+            alt="card-logo"
+            className="w-[50px] absolute top-4 left-4"
+          />
+          {/* card container */}
+          <div>
+            <img src={card_front} />
+          </div>
+        </div>
+        {/* back card */}
+        <div></div>
+      </div>
+    </div>
   );
 }
