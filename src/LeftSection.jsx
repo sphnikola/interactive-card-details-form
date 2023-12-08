0d420a17ffaebbe58845cbc7b9298801c7ae3583
sphnikola <sphnikola@gmail.com>
2023-12-08 14:45:51 +0000

    Add info onto the cards for all screen display

--- src/LeftSection.jsx
+++ src/LeftSection.jsx
@@ -9,24 +9,42 @@ export default function LeftSection() {
     md:min-h-screen md:w-[40%] md:bg-bg-main-desktop "
     >
       {/* container for the cards  */}
-      <div className="flex flex-col-reverse absolute left-[61%] top-[60%] -translate-x-[50%] -translate-y-[50%]">
-        {/* front card */}
-        <div className="z-50 w-[280px] relative -translate-x-[100px]">
+      <div
+        className="flex flex-col-reverse absolute left-[61%] top-[60%] -translate-x-[50%] -translate-y-[50%]
+      md:flex-col md:top-[50%] md:left-[100%] md:gap-8"
+      >
+        {/* front card container */}
+        <div className="z-50 w-[280px] relative -translate-x-[100px] md:w-[300px]">
           {/* card logo */}
           <img
             src={card_logo}
             alt="card-logo"
             className="w-[50px] absolute top-4 left-4"
           />
-          {/* card container */}
+
           <div>
             {/* image for the front of the card */}
             <img src={card_front} />
+            <h1 className="text-lg absolute bottom-[5rem] left-[1rem] tracking-[2px]">
+              0000 0000 0000 0000
+            </h1>
+            {/* front card name and expiry */}
+            <div className="flex justify-between absolute bottom-[2rem] left-[1rem] right-[3rem]">
+              {/* name */}
+              <span>juliana robbins</span>
+              {/* date */}
+              <span>
+                {/* span for month */}
+                <span>00</span> /{/* span for year */}
+                <span>00</span>
+              </span>
+            </div>
           </div>
         </div>
         {/* back card */}
-        <div className="w-[280px] translate-y-[70px] relative">
+        <div className="w-[280px] translate-y-[70px] relative md:w-[300px]">
           <img src={card_back} />
+          <span className="absolute top-[42%] right-[12%]">000</span>
         </div>
       </div>
     </div>
